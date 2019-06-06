dfb67f18c1fed679cdb36a394e205a85bc666994
maguhiro <maguhiro@gmail.com>
2019-06-06 23:34:49 +0900

    Initial GithubMain, GithubCommon project commit

---
+++ GithubCommon/GithubCommon/GithubCommon.h
@@ -0,0 +1,19 @@
+//
+//  GithubCommon.h
+//  GithubCommon
+//
+//  Created by maguhiro on 2019/06/06.
+//  Copyright © 2019 maguhiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GithubCommon.
+FOUNDATION_EXPORT double GithubCommonVersionNumber;
+
+//! Project version string for GithubCommon.
+FOUNDATION_EXPORT const unsigned char GithubCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubCommon/PublicHeader.h>
+
+
---
+++ GithubMain/GithubMain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubMain
+//
+//  Created by maguhiro on 2019/06/06.
+//  Copyright © 2019 maguhiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

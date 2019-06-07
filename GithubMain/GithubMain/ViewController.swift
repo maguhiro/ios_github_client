4a38bf4bf16cf633c6dca725aae2c8635f7f85e8
maguhiro <maguhiro@gmail.com>
2019-06-07 21:10:26 +0900

    apply code format

--- GithubMain/GithubMain/ViewController.swift
+++ GithubMain/GithubMain/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  GithubMain
-//
-//  Created by maguhiro on 2019/06/06.
-//  Copyright © 2019 maguhiro. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
 }
-

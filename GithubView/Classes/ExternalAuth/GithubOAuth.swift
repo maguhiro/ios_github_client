0ac138b0555822cf04ead7980f7d9928a3210936
maguhiro <maguhiro@gmail.com>
2019-06-09 11:36:57 +0900

    create github oauth class

---
+++ GithubView/Classes/ExternalAuth/GithubOAuth.swift
@@ -0,0 +1,32 @@
+import OAuthSwift
+
+public final class GithubOAuth {
+  public static let shared = GithubOAuth()
+
+  private let oauth = OAuth2Swift(consumerKey: GithubOAuthConstants.clientID,
+                                  consumerSecret: GithubOAuthConstants.clientSecret,
+                                  authorizeUrl: "https://github.com/login/oauth/authorize",
+                                  accessTokenUrl: "https://github.com/login/oauth/access_token",
+                                  responseType: "token")
+
+  public func authorize() {
+    oauth.authorize(withCallbackURL: GithubOAuthConstants.callbackURL,
+                    scope: GithubOAuthConstants.scope,
+                    state: GithubOAuthConstants.state,
+                    success: { credential, _, _ in
+                      print(credential.oauthToken)
+                    },
+                    failure: { error in
+                      print(error.localizedDescription)
+    })
+  }
+
+  public func handle(url: URL) -> Bool {
+    if url.scheme == GithubOAuthConstants.callbackURL.scheme {
+      OAuthSwift.handle(url: url)
+      return true
+    }
+
+    return false
+  }
+}

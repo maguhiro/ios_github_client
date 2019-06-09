68c7805f2e830414d07afe2b2f95380377c0a544
maguhiro <maguhiro@gmail.com>
2019-06-09 23:42:56 +0900

    change acl

--- GithubView/Classes/ExternalAuth/GithubOAuth.swift
+++ GithubView/Classes/ExternalAuth/GithubOAuth.swift
@@ -3,13 +3,15 @@ import OAuthSwift
 public final class GithubOAuth {
   public static let shared = GithubOAuth()
 
+  private init() {}
+
   private let oauth = OAuth2Swift(consumerKey: GithubOAuthConstants.clientID,
                                   consumerSecret: GithubOAuthConstants.clientSecret,
                                   authorizeUrl: "https://github.com/login/oauth/authorize",
                                   accessTokenUrl: "https://github.com/login/oauth/access_token",
                                   responseType: "token")
 
-  public func authorize() {
+  func authorize() {
     oauth.authorize(withCallbackURL: GithubOAuthConstants.callbackURL,
                     scope: GithubOAuthConstants.scope,
                     state: GithubOAuthConstants.state,

436b200b17f2e61071c2e19dc97dba87c9f8e981
maguhiro <maguhiro@gmail.com>
2019-06-09 23:43:10 +0900

    add action that tap the login button

--- GithubView/Classes/App/Account/Login/VC/LoginViewController.swift
+++ GithubView/Classes/App/Account/Login/VC/LoginViewController.swift
@@ -1,16 +1,24 @@
 import UIKit
 
-class LoginViewController: UIViewController {
-  public init() {
+final class LoginViewController: UIViewController {
+  init() {
     super.init(nibName: nil, bundle: Bundle(for: LoginViewController.self))
   }
-  
+
   @available(*, unavailable)
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+}
+
+// MARK: ユーザーアクション
+
+private extension LoginViewController {
+  @IBAction func didTapLoginBtn() {
+    GithubOAuth.shared.authorize()
   }
 }

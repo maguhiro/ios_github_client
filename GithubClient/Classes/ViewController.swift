05ec1c9a84a883c3d63d0744729e1d3367b3084b
maguhiro <maguhiro@gmail.com>
2019-06-09 11:39:27 +0900

    add button that authorize github

--- GithubClient/Classes/ViewController.swift
+++ GithubClient/Classes/ViewController.swift
@@ -1,8 +1,13 @@
+import GithubView
 import UIKit
 
 class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+  }
+
+  @IBAction func didTapAuthorizationButton() {
+    GithubOAuth.shared.authorize()
   }
 }

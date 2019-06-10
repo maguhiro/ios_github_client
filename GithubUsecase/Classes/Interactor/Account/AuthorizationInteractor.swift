04868c97e7f590a99ee394b5f15e395a607e5726
maguhiro <maguhiro@gmail.com>
2019-06-11 00:05:42 +0900

    change DI initializer

--- GithubUsecase/Classes/Interactor/Account/AuthorizationInteractor.swift
+++ GithubUsecase/Classes/Interactor/Account/AuthorizationInteractor.swift
@@ -1,7 +1,19 @@
-import Foundation
+import DIKit
 
-public final class AuthorizationInteractor: AuthorizationUsecase {
-  let accountRepository: AccountRepository
+public final class AuthorizationInteractor: AuthorizationUsecase, Injectable {
+  public struct Dependency {
+    fileprivate let accountRepository: AccountRepository
+
+    public init(accountRepository: AccountRepository) {
+      self.accountRepository = accountRepository
+    }
+  }
+
+  private let accountRepository: AccountRepository
+
+  public init(dependency: Dependency) {
+    self.accountRepository = dependency.accountRepository
+  }
 
   public init(accountRepository: AccountRepository) {
     self.accountRepository = accountRepository

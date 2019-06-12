cc4e8a6f8b47d2f6246fc32dd5872d26c0e59df4
maguhiro <maguhiro@gmail.com>
2019-06-12 20:53:29 +0900

    set iso8601 dateformat

--- GithubInfra/Classes/Const/Constants.swift
+++ GithubInfra/Classes/Const/Constants.swift
@@ -3,9 +3,12 @@ import GithubUtility
 let log = Logger.shared
 
 enum Constants {
+  // Githubの日付フォーマットは ISO 8601
+  // https://developer.github.com/v3/#schema
   static let decoder: JSONDecoder = {
     let decoder = JSONDecoder()
     decoder.keyDecodingStrategy = .convertFromSnakeCase
+    decoder.dateDecodingStrategy = .iso8601
     return decoder
   }()
 
@@ -13,6 +16,7 @@ enum Constants {
     let encoder = JSONEncoder()
     encoder.keyEncodingStrategy = .convertToSnakeCase
     encoder.outputFormatting = .prettyPrinted
+    encoder.dateEncodingStrategy = .iso8601
     return encoder
   }()
 }

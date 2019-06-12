c2c583752314ba0af7b4a757deda74bbf4f04a92
maguhiro <maguhiro@gmail.com>
2019-06-12 20:51:38 +0900

    implement CustomStringConvertible

--- GithubEntity/Classes/Core/ValueObject.swift
+++ GithubEntity/Classes/Core/ValueObject.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-public protocol ValueObject: Codable {
-  associatedtype Value: Codable
+public protocol ValueObject: Codable, CustomStringConvertible {
+  associatedtype Value: Codable, CustomStringConvertible
 
   var value: Value { get }
 
@@ -18,5 +18,9 @@ public extension ValueObject {
   func encode(to encoder: Encoder) throws {
     var container = encoder.singleValueContainer()
     try container.encode(value)
+  }
+
+  var description: String {
+    return value.description
   }
 }

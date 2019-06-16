1de43ed9e803fc161e3df33755edcc14adec4d98
maguhiro <maguhiro@gmail.com>
2019-06-16 13:01:26 +0900

    エンティティで保持するデータを追加

--- GithubEntity/Classes/Entity/User.swift
+++ GithubEntity/Classes/Entity/User.swift
@@ -3,6 +3,13 @@ import Foundation
 public struct User: Entity {
   public let id: UserID
   public let name: String
+  public let avatarUrl: URL
+  public let publicRepos: Int
+  public let totalPrivateRepos: Int
+  public let publicGists: Int
+  public let privateGists: Int
+  public let followers: Int
+  public let following: Int
   public let createdAt: Date
   public let updatedAt: Date
 }
